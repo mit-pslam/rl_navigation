6a927f01208cb688296bbcfdd42d5ba09eb5df4f
Mark Mazumder <mazumder@ll.mit.edu>
2020-02-27 10:45:43 -0500

    update video generation script

--- fg_video.py
+++ fg_video.py
@@ -8,48 +8,49 @@ from fg import FlightGogglesHeadingEnv
 import time
 import os
 
+from config import get_cfg_defaults
 
-pose_port = "10253"
-video_port = "10254"
-report_port = "5556"
-
-def make_unity_env(num_env):
+def make_unity_env(config, num_env):
     """ Create a wrapped Unity environment. """
 
     def make_env(rank):
         def _thunk():
-            env = FlightGogglesHeadingEnv(pose_port, video_port, report_port)
+            env = FlightGogglesHeadingEnv(config)
             return env
 
         return _thunk
 
     return DummyVecEnv([make_env(i) for i in range(num_env)])
 
+if __name__ == "__main__":
+    cfg = get_cfg_defaults()
+    # cfg.merge_from_file("experiment.yaml")
+    cfg.freeze()
 
-env = make_unity_env(1)
+    env = make_unity_env(cfg, 1)
 
 
-model = PPO2.load("../../fg_policies/loopfg_heading4.policy")
+    model = PPO2.load("loopfg_twist.policy")
 
-video_length = 2000
+    video_length = 6000
 
-video_env = VecVideoRecorder(
-    env,
-    video_folder="videos",
-    record_video_trigger=lambda x: x == 0,
-    video_length=video_length,
-    name_prefix="loopfg",
-)
+    video_env = VecVideoRecorder(
+        env,
+        video_folder="videos",
+        record_video_trigger=lambda x: x == 0,
+        video_length=video_length,
+        name_prefix="loopfg",
+    )
 
-obs = video_env.reset()
-for _ in range(video_length + 1):
-    action, _ = model.predict(obs)
-    obs, _, _, _ = video_env.step(action)
+    obs = video_env.reset()
+    for _ in range(video_length + 1):
+        action, _ = model.predict(obs)
+        obs, _, _, _ = video_env.step(action)
 
-# os.rename(
-#     "/home/mark/relate/habitat_rl/fg/videos/loopfg-step-0-to-step-2500.mp4",
-#     "/home/mark/relate/habitat_rl/fg/videos/loopfg.mp4",
-# )
+    # os.rename(
+    #     "/home/mark/relate/habitat_rl/fg/videos/loopfg-step-0-to-step-2500.mp4",
+    #     "/home/mark/relate/habitat_rl/fg/videos/loopfg.mp4",
+    # )
 
 
 
